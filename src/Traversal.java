ab4dd38ddd32e90d5254dd170f977e6dd2afeb40
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-02-06 14:48:52 -0800

    Added some code

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,31 @@
 public class Traversal {
   public static void main(String[] args) {
+    
+    TreeNode root = new TreeNode(77,null, null);
+    root.left = new TreeNode(22, null, null);
+    root.right = new TreeNode(86, null, null);
+    root.left.right = new TreeNode(33, null, null);
 
+    preOrder(root);
+    postOrder(root);
+
+  }
+
+  public static void preOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+    System.out.println(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+
+  public static void postOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+    postOrder(node.left);
+    postOrder(node.right);
+    System.out.println(node.value);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,17 @@
 public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
 
+    // Constructor
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    
+
+    
   
 }
